ed4028ee97111ff509261af028b2703f5ebd6c48
Keni Borges <keni@questores.com.br>
2022-07-30 08:19:51 -0300

    all

--- MVC/Escola/Escola/Repositorios/BaseRepository.cs
+++ MVC/Escola/Escola/Repositorios/BaseRepository.cs
@@ -1,8 +1,5 @@
 ﻿using Escola.Context;
 using Escola.Repositorios.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Escola.Repositorios
 {
